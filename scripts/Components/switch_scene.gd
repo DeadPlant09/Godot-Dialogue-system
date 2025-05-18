03fa1ea28fe0db0845d1309e220c3eb580643216
DeadPlant09 <143194253+DeadPlant09@users.noreply.github.com>
2025-05-17 22:22:30 -0400

    fixed circuler refrence error
    i hade to lode the scene path after the
    main scene started

--- scripts/Components/switch_scene.gd
+++ scripts/Components/switch_scene.gd
@@ -1,10 +1,10 @@
 extends Area2D
 class_name Switch_Sceane
 
-@export var Scene:PackedScene
+@export var Scene:String
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.has_method("player"):
-		get_tree().call_deferred("change_scene_to_packed", Scene)# "call_deferred" run's the function at the end of the signal as to not cause errors when emedeitly removing a colision object from an area. 
+		get_tree().call_deferred("change_scene_to_packed", load(Scene))# "call_deferred" run's the function at the end of the signal as to not cause errors when emedeitly removing a colision object from an area. 
 		
 	
