185c0e68d86acbb8f9dfbf3f4b2433ce7f5b1f0f
DeadPlant09 <143194253+DeadPlant09@users.noreply.github.com>
2025-07-12 17:38:04 -0400

    fixed and added cutscene dialogue
    added a function to play cutscene dialogue, added the reaction icon and fixed it reseting, added more options like pausing at indexes of changing volume, added Profile (icon) animations.

--- scripts/Components/Cutscene trigger.gd
+++ scripts/Components/Cutscene trigger.gd
@@ -2,15 +2,34 @@ extends Area2D
 
 @export var Run_at_start:bool 
 @export var json_path:String = "res://Resourses/MAIN.json"
+@export var Index:int = 1
 @export var Conversation:int = 1
+var Cutscene_ran = false
+#constants
+const Save_Path = "res://Resourses/save.cfg"
 
+# variables
+var config = ConfigFile.new()
+# Node variables
+
+func Save_cutscene():
+	config.set_value("cutscene","cutscene_ran_" + str(Index), Cutscene_ran)
+	config.save(Save_Path)
+
+func Load_load():
+	var error = config.load(Save_Path) # using the save path to load the save file 
+	if error != OK: return
+	Cutscene_ran = config.get_value("cutscene","cutscene_ran_" + str(Index), false) # find the value of high score from the save file, if it can't find the high score value set it to the game default: 0 
 
 func _ready() -> void:
+	Load_load()
 	if Run_at_start:
 		Diauloge_Systerm.Run_dialouge(json_path, Conversation)
 
 func _process(delta: float) -> void:
 	for body in get_overlapping_bodies():
-		if body.has_method("player"):
+		if body.has_method("player") and not Cutscene_ran:
 			Diauloge_Systerm.Run_dialouge(json_path, Conversation)
-			collision_mask = 0
+			await Diauloge_Systerm.dialogue_finished
+			Cutscene_ran = true # run once in the same scene, you can change on variable in a save file without editing the rest which is what you can do in this instance with your save system in your own projects
+			Save_cutscene()
--- scripts/UI/dialogue_systerm.gd
+++ scripts/UI/dialogue_systerm.gd
@@ -7,10 +7,11 @@ signal dialogue_finished
 @onready var sprites: Control = $"Dialogue UI/Sprites"
 @onready var overlap_detection: Area2D = $"Overlap Detection"
 @onready var Character_Text = $"Dialogue UI/RichTextLabel"
-@onready var Character_Icon = %Icon
+@onready var profile_animations: AnimationPlayer = $"Profile animations"
 @onready var Character_Voice: AudioStreamPlayer2D = $"Dialogue UI/Voice"
 @onready var options = $"Dialogue UI/Options"
-@onready var Reaction: RichTextLabel = $"Dialogue UI/Response"
+@onready var react_profile = $"Dialogue UI/React Profile"
+@onready var reaction_text = $"Dialogue UI/Reaction text"
 @onready var show_responce: AnimationPlayer = $"Show Responce"
 
 # variables
@@ -30,7 +31,8 @@ var Choice_Aftermath:Array
 var Deactivated = false
 var is_dialogue_runing = true
 var has_dialogue_ran = false
-@export var Icon_path = "res://Sprites/Character's/"
+var Profile_index = 1
+@export var Profile_path = "res://Sprites/Character's/"
 @export var Voice_path = "res://Audio/"
 # option variables
 @export var Debug_output:bool 
@@ -40,8 +42,10 @@ var skipable = true
 var play_voice = true
 var auto_skip = false
 var wait_for_chocies = false
+var pause_at_character = [",", "-", ";", ":", ".", "?", "!"]
 var pause_at_ending_of_sentence = true
-var silent_characters = ["!",",", "-", ".", "?",";", '"', "©", "™", "[", "]"]
+var pause_at_index = []
+var silent_characters = [" ","!",",", "-", ".", "?",";", '"', "©", "™", "[", "]"]
 var just_show_text = false
 
 func _ready() -> void:
@@ -62,6 +66,7 @@ func start_dialogue() -> void:
 		return
 	
 	sprites.visible = not just_show_text
+	show_responce.play("RESET") # so the responce hides when you run the same dialouge
 	show()
 	Deactivated = true # so next time it enters the function it wit imeditly exit
 	Dialogue = load_Json(Json_file)
@@ -103,27 +108,49 @@ func Next_Dialogue():
 	
 	is_dialogue_runing = true
 	has_dialogue_ran = false
-	Set_Profile(Character_Icon)
+	
+	# seting up dialogue
+	set_up_dialogue_options()
+	Set_Profile()
 	set_text()
 
-func Set_Profile(image_node: TextureRect):
+func set_up_dialogue_options():
+	Profile_index = int(Dialogue[str(conversation_id)][current_diauogue_id].get('Face', 0)) 
+	Character_Voice.volume_db = Dialogue[str(conversation_id)][current_diauogue_id].get('volume', 0.0)
+	skipable = Dialogue[str(conversation_id)][current_diauogue_id].get('skipable', true)
+	play_voice = Dialogue[str(conversation_id)][current_diauogue_id].get('voice', true)
+	auto_skip = Dialogue[str(conversation_id)][current_diauogue_id].get('Auto Skip', false)
+	wait_for_chocies = Dialogue[str(conversation_id)][current_diauogue_id].get('Choices')
+	pause_at_ending_of_sentence =  Dialogue[str(conversation_id)][current_diauogue_id].get('pause at ending', true)
+	just_show_text = Dialogue[str(conversation_id)][current_diauogue_id].get('just text', false)
+	reaction_text.text =  Dialogue[str(conversation_id)][current_diauogue_id].get('reactions', ["",0,""])[2]
+	pause_at_index = Dialogue[str(conversation_id)][current_diauogue_id].get('pause')
+	
+
+func Set_Profile(): # runs befor options are set
 	Character_Text.visible_ratio = 0 # when you change your character profile reste the visable text to 0
 	# reset the postion and size to defaults 
-	Character_Text.position.x = 320
-	Character_Text.size.x = 608
 	Character_Voice.stream = load(Voice_path + "Default dialogue voice.wav")
+	#print("reset")
 	
 	if FileAccess.file_exists(Voice_path + str(Dialogue[str(conversation_id)][current_diauogue_id]['Name']) + " voice.wav"):
 		Character_Voice.stream = load(Voice_path + str(Dialogue[str(conversation_id)][current_diauogue_id]['Name']) + " voice.wav")
 	
-	if FileAccess.file_exists(Icon_path + str(Dialogue[str(conversation_id)][current_diauogue_id]['Name']) + ".svg"):
-		image_node.texture = load(Icon_path + str(Dialogue[str(conversation_id)][current_diauogue_id]['Name']) + ".svg")
-	elif FileAccess.file_exists(Icon_path + str(Dialogue[str(conversation_id)][current_diauogue_id]['Name']) + ".png"):
-		image_node.texture = load(Icon_path + str(Dialogue[str(conversation_id)][current_diauogue_id]['Name']) + ".png")
-	else:
-		image_node.texture = null 
-		Character_Text.position.x = 208
-		Character_Text.size.x = 720
+	for animation in profile_animations.get_animation_list():
+		var Full_name = Dialogue[str(conversation_id)][current_diauogue_id]['Name'] + str(Profile_index)
+		if animation.contains(Full_name):
+			#print(Full_name)
+			Character_Text.position.x = 320
+			Character_Text.size.x = 608
+			profile_animations.play(Full_name, -1, 0.0)
+			break # so it donet cheak for other animations after it found the spisific one
+		else:
+			#print("Full_name")
+			profile_animations.play("RESET")
+			Character_Text.position.x = 208
+			Character_Text.size.x = 720
+			
+			
 
 func set_text():
 	if Dialogue[str(conversation_id)][current_diauogue_id].get('Screen position', []):# if that 'Screen_position' doesnt exist it will return null
@@ -132,11 +159,7 @@ func set_text():
 	
 		
 	Character_Text.text = Dialogue[str(conversation_id)][current_diauogue_id]['Text'] 
-	
-	if not Dialogue[str(conversation_id)][current_diauogue_id].get('Speed'):
-		scrolling_text(Character_Text) 
-	else:
-		scrolling_text(Character_Text, Dialogue[str(conversation_id)][current_diauogue_id]['Speed']) # make the charcter text scroll after you set it
+	scrolling_text(Character_Text, Dialogue[str(conversation_id)][current_diauogue_id].get('Speed', 0.05)) # make the charcter text scroll after you set it
 	
 
 
@@ -144,34 +167,44 @@ func scrolling_text(text_node:RichTextLabel, Speed = 0.05):
 	var voice_played = 0
 	var parsed_text_length = text_node.get_parsed_text().length()
 	for e in text_node.get_text(): # scroling text, for every letter in character text
-		text_node.visible_characters += 1 # make one character visable
+		text_node.visible_characters += 1 # make one character visable (visable text includes spaces)
 		if is_dialogue_runing:
-			set_up_dialogue_options()
-			if not text_node.visible_characters == 0 and e == ","  or e == "-" or e == ";" or e == ":": # Small pasues at beginging or middle of sentece
-				# if the character is these letter wait a little more
-				await get_tree().create_timer(0.1).timeout
+			profile_animations.stop()
 			
-			if pause_at_ending_of_sentence and e == "." or e == "?" or e == "!": # Long pasues at the End of sentece
-				# if the character is these letter wait a little more
-				await get_tree().create_timer(0.3).timeout
+			
+			if pause_at_index != null:
+				for p in pause_at_index: # for ever value in "pause_at_index"
+					if int(p) == text_node.visible_characters: # if the index is the amount of visab;e characters shown 
+						await get_tree().create_timer(0.3).timeout # then wait for 0.3 sec 
+			
+			for s in pause_at_character:
+				if pause_at_ending_of_sentence and e == s: # Long pasues at the End of sentece
+					# if the character is these letter wait a little more
+					await get_tree().create_timer(0.3).timeout
 			
 			for c in silent_characters:
-				if e == c: play_voice = false
+				if e == c:
+					play_voice = false
+			
+			if play_voice == true: profile_animations.play(profile_animations.current_animation) 
 			
 			if play_voice == true and voice_played <= parsed_text_length: # to make sure the sound doenst play when counting the bbcode text 
 				voice_played += 1
 				Character_Voice.play()
 			
+			play_voice = true
+		if Debug_output:print(e)
 		await get_tree().create_timer(Speed).timeout #every  0.05 seconds times the length of the text 
 		
-		if text_node.visible_characters >= parsed_text_length or Input.is_action_pressed("Skip_Text") and skipable: # if the skip text input was pressed
+		if text_node.visible_characters >= parsed_text_length or Input.is_action_pressed("Skip_Text") and skipable: # if the text is skiped or all the parse text it shown
 			text_node.visible_characters = text_node.text.length() # set the visable characters to the full length
-			break
+			break # exit out of for loop
 	
-	if text_node.visible_characters == text_node.text.length() or text_node.visible_characters == parsed_text_length: # if all the parsed text is showen.
+	if text_node.visible_characters == text_node.text.length(): # if all the text is showen.
+		if Debug_output: print("finised")
 		# if that 'Responses' doesnt exist it will return null
 		if not current_diauogue_id  >= len(Dialogue[str(conversation_id)]) and Dialogue[str(conversation_id)][current_diauogue_id].get('Choices'):# when pressing the skip button to fast it crashesx
-			if Debug_output: (Dialogue[str(conversation_id)][current_diauogue_id]['Choices'])
+			if Debug_output: print(Dialogue[str(conversation_id)][current_diauogue_id]['Choices'])
 			choices_exsist = true
 			Choice_Responces = Dialogue[str(conversation_id)][current_diauogue_id]['Responses']
 			Choice_Aftermath = Dialogue[str(conversation_id)][current_diauogue_id]['Aftermath']
@@ -179,8 +212,12 @@ func scrolling_text(text_node:RichTextLabel, Speed = 0.05):
 			
 		is_dialogue_runing = false
 		has_dialogue_ran = true
+		
 		# options
-		if not Reaction.text == "": show_responce.play("slide in")
+		if not reaction_text.text == "":
+			if FileAccess.file_exists(Profile_path + str(Dialogue[str(conversation_id)][current_diauogue_id]['reactions'][0]) + " Profile.png"):
+				react_profile.texture = load(Profile_path + str(Dialogue[str(conversation_id)][current_diauogue_id]['reactions'][0]) + " Profile.png")
+			show_responce.play("slide in")
 		if auto_skip: Next_Dialogue()
 	
 
@@ -200,7 +237,7 @@ func show_choices():
 		var index = b.get_index()
 		if b is Button:
 			options.get_child(index).visible = false
-			if not Choices[index] == "": # Make sure theres a Reaction
+			if not Choices[index] == "": # Make sure theres a reaction_text
 				options.show()
 				options.get_child(index).visible = true
 				options.get_child(index).text = Choices[index]
@@ -242,13 +279,3 @@ func change_to_choice_dialouge(choice): # cannot get path to 'Responses' in  thi
 func _on_overlap_detection_area_exited(area: Area2D) -> void:
 	if visible == false:
 		dialogue_ui.position = Vector2.ZERO
-
-func set_up_dialogue_options():
-	Character_Voice.volume_db = Dialogue[str(conversation_id)][current_diauogue_id].get('volume', 0.0)
-	skipable = Dialogue[str(conversation_id)][current_diauogue_id].get('skipable', true)
-	play_voice = Dialogue[str(conversation_id)][current_diauogue_id].get('voice', true)
-	auto_skip = Dialogue[str(conversation_id)][current_diauogue_id].get('Auto Skip', false)
-	wait_for_chocies = Dialogue[str(conversation_id)][current_diauogue_id].get('Choices')
-	pause_at_ending_of_sentence =  Dialogue[str(conversation_id)][current_diauogue_id].get('pause at ending', true)
-	just_show_text = Dialogue[str(conversation_id)][current_diauogue_id].get('just text', false)
-	Reaction.text =  Dialogue[str(conversation_id)][current_diauogue_id].get('reactions', "")
